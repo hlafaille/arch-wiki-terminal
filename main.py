436c4db7cab4dc8788a9f3e1b2a2d17ef32f6ff9
hunter <watermelonmcmuffintv@gmail.com>
2022-03-25 14:10:06 -0700

    rebuilt the entire system in beautifulsoup instead of using my own shit parser

--- main.py
+++ main.py
@@ -1,61 +1,54 @@
-import requests as requests
+import os
 
-from html_parser import table_of_contents, header, optional_features_list, get_section
+import requests
+from bs4 import BeautifulSoup
 
-class PageHandler:
-    def __init__(self):
-        self.page = None
-        self.sections = {}
-
-    def user_input(self):
-        # Get the user input
-        user_input = input("search> ")
-
-        print("searching...")
-
-        # Get the HTML of the page
-        self.page = requests.get("https://wiki.archlinux.org/index.php?search={0}".format(user_input)).text
-
-        self.root_page()
-
-    def root_page(self,):
-        # Get the various elements from the page
-        header_text = header.ParseHeader(self.page)
-        body_list = optional_features_list.BodyList(self.page).parse()
-        self.sections = table_of_contents.ParseTableOfContents(self.page).parse()
-
-        if not header_text.parse() == "Search results":
-            print("-------------------------------")
-            # Print the page out to stdout
-            print(header_text.parse())
-            if body_list:
-                print("-------------------------------")
-                for bullet in body_list:
-                    print("- {0}".format(bullet))
-            print("-------------------------------")
-            for content in self.sections:
-                if content["level"].count(".") == 1:
-                    print("   {0} --- {1}".format(content["level"], content["text"]))
-                elif content["level"].count(".") == 2:
-                    print("      {0} --- {1}".format(content["level"], content["text"]))
-                else:
-                    print("{0} --- {1}".format(content["level"], content["text"]))
-            print("-------------------------------")
-            self.get_section()
-        else:
-            print("nothing was found")
-            self.user_input()
-
-    def get_section(self):
-        # Ask the user which section they want
-        section = input("section> ")
-
-        # If the user wants to re-search something
-        if section == "e":
-            self.user_input()
-        else:
-            specific_section = get_section.GetSection(self.page, self.sections, section)
-            specific_section.parse()
 if __name__ == "__main__":
-    page_handler = PageHandler()
-    page_handler.user_input()
+    # ask for search
+    user_search = input("search> ")
+
+    # get data from arch wiki
+    data = requests.get("https://wiki.archlinux.org/index.php?search={0}".format(user_search))
+
+    # set up beautifulsoup
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # get and print the header
+    main_header = soup.find("h1", {"class": "firstHeading"})
+    print(main_header.text)
+    print("--------------------")
+
+    # get the main content
+    main_content = soup.find("main", {"id": "content"})
+
+    # get the big boy paragraph
+    try:
+        big_boy_paragraph = soup.find("dd")
+        print(big_boy_paragraph.text)
+        print("--------------------")
+    except AttributeError:
+        print("...")
+
+    # get all h2 headings in the main content
+    page_headers = main_content.find_all("h2", id=None)
+
+    headers_count = 0
+    for header in page_headers:
+        if not header.text == "Contents":
+            headers_count += 1
+            print("[{0}] {1}".format(headers_count, header.text))
+
+    user_input = int(input("> "))
+    #print("user input is {0}, getting next next_element.text until next element text is {1}".format(user_input, page_headers[user_input].text))
+
+    print("--------------------")
+
+    # get all the text between this h2 and the next h2
+    next_tag = page_headers[user_input - 1].find_all_next(['p', "span"])
+
+    for x in next_tag:
+        if x.name == "span" and x.text == page_headers[user_input].text:
+            print("breaking")
+            break
+        else:
+            print(x.text)
